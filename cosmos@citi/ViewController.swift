97a906275e3670646049f7bb4a345b225e4adece
vaishnavi kothavasal srinivasa <kookoo@vaishnavis-macbook-pro.local>
2018-10-25 09:16:42 -0400

    Initial Commit

---
+++ cosmos@citi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cosmos@citi
+//
+//  Created by vaishnavi kothavasal srinivasa on 10/24/18.
+//  Copyright © 2018 ICG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
